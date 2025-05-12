d405d28523411fa6a4540927230c911a5b376de7
Erwan Menard <erwan.menard@ynov.com>
2025-05-12 13:41:31 +0200

    corps

--- Option_corps/corps.py
+++ Option_corps/corps.py
@@ -1,32 +1,102 @@
-import kinematics
-from utils import SimpleRobotSimulation
+from utils import SimpleRobotSimulation, Parameters
 from onshape_to_robot.simulation import Simulation
+import kinematics
+import utils
+
+import time
+import math
 import pybullet as p
+from scipy.spatial.transform import Rotation
+from controle import Controle
+
 
 class Corps:
     def __init__(self):
         self.robotPath = "phantomx_description/urdf/phantomx.urdf"
         self.sim = Simulation(self.robotPath, gui=True, panels=True, useUrdfInertia=False)
-        self.sim.setRobotPose([0, 0, 0.5], p.getQuaternionFromEuler([0, 0, 0]))
+        self.sim.setRobotPose([0, 0, 0.5], self.to_pybullet_quaternion(0, 0, 0, degrees=True))
         self.robot = SimpleRobotSimulation(self.sim)
         self.robot.init()
         print(self.robot)
 
+        # Ajout des curseurs pour ajuster les paramètres de marche
+        self.slider_amplitude = p.addUserDebugParameter("Amplitude", 0.01, 0.15, 0.08)
+        self.slider_height = p.addUserDebugParameter("Hauteur", 0.01, 0.1, 0.05)
+
+
+        self.params = Parameters(
+            freq=50,
+            speed=1,
+            z=80,
+            travelDistancePerStep=80,
+            lateralDistance=110,
+            frontDistance=87,
+            frontStart=32,
+            method="minJerk",
+        )
+
+    def to_pybullet_quaternion(self, roll, pitch, yaw, degrees=False):
+        rot = Rotation.from_euler("xyz", [roll, pitch, yaw], degrees=degrees)
+        return rot.as_quat()
+
     def run(self):
-        x_value = float(input("Ecris la valeur de x :"))
-        y_value = float(input("Ecris la valeur de y :"))
-        z_value = float(input("Ecris la valeur de z :"))
-        print("Voilà les coordonnées :", x_value, y_value, z_value)
 
-        while True :
-            for l in range(1,7):
-                thetas = kinematics.computeIKOriented(x_value, y_value, z_value, None, 0)
+        # Récupération des paramètres dynamiques via les curseurs
+        extra_angle = 0
 
-                for m in range(0,3):
-                    self.robot.legs[l][m].goal_position = thetas[m]
+        mode_rotation = 0  # 0: normal, -1: gauche, +1: droite
+        mode_deplacement = False
+        index_patte = [1, 2, 3, 4, 5, 6]
+        is_idle = True  # Robot au repos au départ
 
-                self.sim.tick()
-                self.robot.tick_read()
+        utils.setPositionToRobot(0, 0, 0.05, self.robot, self.params, extra_theta=0)
+        self.robot.smooth_tick_read_and_write(1, verbose=True)
+        while True:
+            amplitude = p.readUserDebugParameter(self.slider_amplitude)
+            height = p.readUserDebugParameter(self.slider_height)
 
-                if round(self.robot.legs[l][0].present_position, 4) == round(thetas[0],4) and round(self.robot.legs[l][1].present_position, 4) == round(thetas[1], 4) and round(self.robot.legs[l][2].present_position, 4) == round(thetas[2], 4):
-                    break
+            keys = p.getKeyboardEvents()
+
+            mode_rotation, mode_deplacement, extra_angle, is_idle = Controle.handle_keys(keys)
+
+
+            # Groupes de jambes
+            index_patte1 = [1, 3, 5]  # phase 0
+            index_patte2 = [2, 4, 6]  # phase 1
+
+            # Calcul de la position des jambes selon les paramètres dynamiques
+            if mode_deplacement:
+                for l in index_patte1:
+                    thetas = kinematics.triangle(0, -0.05, height, amplitude, self.sim.t, leg_id=l, extra_angle=extra_angle)
+
+                    for m in range(3):
+                        self.robot.legs[l][m].goal_position = thetas[m]
+
+                for l in index_patte2:
+                    thetas = kinematics.triangle(0, -0.05, height, amplitude, self.sim.t , leg_id=l, extra_angle=extra_angle)
+                    for m in range(3):
+                        self.robot.legs[l][m].goal_position = thetas[m]
+
+            elif mode_rotation != 0:
+                # Rotation
+                t_inverse = self.sim.t  * mode_rotation
+
+                for l in index_patte1:
+                    thetas = kinematics.triangle(0.2, -0.10, height, amplitude, t_inverse)
+                    for m in range(0, 3):
+                        self.robot.legs[l][m].goal_position = thetas[m]
+
+                for l in index_patte2:
+                    thetas = kinematics.triangle(0.2, -0.10, height, amplitude, t_inverse + 1)
+                    for m in range(0, 3):
+                        self.robot.legs[l][m].goal_position = thetas[m]
+
+            else:
+                # Repos
+                utils.setPositionToRobot(0, 0, 0.05, self.robot, self.params, extra_theta=0)
+                        
+
+            # self.robot.smooth_tick_read_and_write(0.01, verbose=True)
+            self.robot.tick_read_and_write()
+
+            self.sim.tick()
